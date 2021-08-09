30f7c1a3c669600c07aa2e2d55ef0a3e4b4359e1
Cyrus Chan <cyruschan4561@gmail.com>
2021-08-09 15:32:29 -0700

    Purposely failing unit test to test GitHub Actions CI

--- tests/test_filetype_cmd.py
+++ tests/test_filetype_cmd.py
@@ -11,7 +11,7 @@ def test_filetype_response():
     assert filetype_cmd.display_file_information("exe") == 'An EXE file contains an executable program for Windows.  EXE is short for "executable," and it is the standard file extension used by Windows programs.  For many Windows users, EXE files are synonymous with Windows programs, making ".exe" one of the most recognizable file extensions.'
 
     assert filetype_cmd.display_file_information(".sln") == 'An SLN file is a structure file used for organizing projects in Microsoft Visual Studio.  It contains text-based information about the project environment and project state.'
-    assert filetype_cmd.display_file_information("sln") == 'An SLN file is a structure file used for organizing projects in Microsoft Visual Studio.  It contains text-based information about the project environment and project state.'
+    assert filetype_cmd.display_file_information("sln") == 'An SLN file i a structure file used for organizing projects in Microsoft Visual Studio.  It contains text-based information about the project environment and project state.'
 
     error_message = "```Error. Example of proper usage:\n\n%filetype .exe```"
 
